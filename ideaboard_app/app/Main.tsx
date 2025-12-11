5dc1797c9d3e71e2eae5fdb638f4c3dd01b8bd3f
CodeFummel <nekofumiii@gmail.com>
2025-12-11 21:07:21 +0100

    Change main Menu to use Ant Design (Bugged)

--- ideaboard_app/app/Main.tsx
+++ ideaboard_app/app/Main.tsx
@@ -1,32 +1,54 @@
 "use client"
 
 import {useState} from "react";
-import { Button, DatePicker } from 'antd';
+import {Button, Dropdown, MenuProps, Tabs} from 'antd';
+import {FilterOutlined} from "@ant-design/icons";
 
 export const Main = () => {
-  const [ideas, setIdeas] = useState(["Idea 1", "Idea 2", "Idea 3"]);
-  const newIdea = (idea) => {
-    setIdeas((ideas) => [...ideas, idea])
-  };
 
-  return(
-    <main className="min-h-[0] p-[8px] m-[8px] text-left flex flex-col flex-2 justify-start gap-(--flex-gap) border-2 border-solid rounded-(--border-radius) border-(--border)">
-    <div className="flex justify-between">  
-        <nav className="">
-          <button className="navButton hover:bg-(--hover-background)">Beste Ideen</button>
-          <button className="navButton hover:bg-(--hover-background)">Neue Ideen</button>
-          <button className="navButton hover:bg-(--hover-background)">Projekte</button>
-          <button className="navButton hover:bg-(--hover-background)">Umfragen</button>
-          <Button type="primary">Button</Button>
-        </nav>
-        <div className="">
-          <input></input>
-          <button onClick={() => newIdea("Hallo")} className="navButton hover:bg-(--hover-background)">Idee erstellen</button>
-        </div>
-    </div>
-      <div className="h-max p-[8px] m-[8px] text-left justify-start border-2 border-solid rounded-(--border-radius) border-(--border)">
-      {ideas.map((idea, i)=><p key={i}>{idea}</p>)}
-      </div>
-    </main>
-  );
+    const createIdeaButton = <Button type={"primary"} onClick={() => newIdea("Hallo")}>Idee erstellen</Button>
+    const [ideas, setIdeas] = useState(["Idea 1", "Idea 2", "Idea 3"]);
+    const newIdea = (idea) => {
+        setIdeas((ideas) => [...ideas, idea])
+    };
+    const items = [
+        {
+            label: "Ideen",
+            key: "ideas-tab",
+            children: <div
+                className="h-max p-[8px] m-[8px] text-left justify-start border-2 border-solid rounded-(--border-radius) border-(--border)\">
+                {ideas.map((idea, i) => <p key={i}>{idea}</p>)}
+            </div>
+        },
+        {label: "Projekte", key: "projects-tab", children: "Projekte yippie"},
+        {label: "Umfragen", key: "polls-tab", children: "Umfragen yippie"},
+    ];
+    const filterOptions: MenuProps["items"] = [
+        {
+            label: "Best",
+            key: "1",
+        },
+        {
+            label: "New",
+            key: "2",
+        },
+        {
+            label: "Old",
+            key: "3",
+        },
+    ];
+    const filterButton = <Dropdown trigger={["click"]} menu={{items: filterOptions}}><FilterOutlined/></Dropdown>
+
+
+    return (
+        <main
+            className="min-h-[0] p-[8px] m-[8px] text-left flex flex-col flex-2 justify-start gap-(--flex-gap) border-2 border-solid rounded-(--border-radius) border-(--border)">
+            <div className="flex justify-between">
+                <nav className="flex flex-1">
+                    <Tabs type={"card"} items={items} defaultActiveKey={"ideas-tab"}
+                          tabBarExtraContent={{left: filterButton, right: createIdeaButton}}/>
+                </nav>
+            </div>
+        </main>
+    );
 };
