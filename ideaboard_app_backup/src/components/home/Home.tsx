d3f38fc6eca8abdba28579736ca3fde2655885e7
Kaliexe321 <justus.schindler@icloud.com>
2026-02-26 19:31:15 +0100

    Add Tiles and Darkmode

--- ideaboard_app_backup/src/components/home/Home.tsx
+++ ideaboard_app_backup/src/components/home/Home.tsx
@@ -7,13 +7,13 @@ import {TopThreeIdeas} from "@/src/components/home/TopThreeIdeas";
 export const Home = () => {
     return (
         <div className={"grid grid-cols-3 grid-rows-2 gap-4 h-dvh m-4 mt-0"}>
-            <div className={"row-span-2 h-full"}>
+            <div className={"row-span-2 h-full shadow-lg"}>
                 <MyIdeas/>
             </div>
-            <div className={"row-span-2 h-full"}>
+            <div className={"row-span-2 h-full shadow-lg"}>
                 <MyProjects/>
             </div>
-            <div className={"row-span-2 h-full"}>
+            <div className={"row-span-2 h-full shadow-lg"}>
                 <TopThreeIdeas/>
             </div>
         </div>
