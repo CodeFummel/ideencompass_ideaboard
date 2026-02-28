4850ff002ca58cdb798a26b4a8c0e3b26d54d021
CodeFummel <nekofumiii@gmail.com>
2026-03-01 00:23:18 +0100

    Fix Header

--- ideaboard_app/src/components/head/Header.tsx
+++ ideaboard_app/src/components/head/Header.tsx
@@ -3,9 +3,14 @@
 import { UserButton } from "@/src/components/head/UserButton"
 import { Switch } from "antd"
 import { useTheme } from "@/src/theme/ThemeProvider"
+import {authClient} from "@/src/utils/auth-client";
 
 export const Header = () => {
     const { darkMode, toggle } = useTheme()
+
+    const {
+        data: session,
+    } = authClient.useSession()
 
     return (
         <header className="flex flex-row gap-4 items-center border-b-2 border-solid border-(--border) ml-4 mr-4 h-20">
@@ -13,15 +18,13 @@ export const Header = () => {
 
             <div className="flex flex-1 justify-end items-center gap-4 h-full">
 
-                {/* Dark Mode Toggle */}
                 <Switch
                     checked={darkMode}
                     onChange={toggle}
                     checkedChildren="🌙"
                     unCheckedChildren="☀️"
                 />
-
-                <UserButton />
+                {session ? <UserButton /> : null}
             </div>
         </header>
     )
