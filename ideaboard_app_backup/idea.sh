26ea5f969d008f5a6f00db7bfd848a5ae100808f
Kaliexe321 <justus.schindler@icloud.com>
2026-02-28 20:32:51 +0100

    deleting unimportant files

--- ideaboard_app_backup/idea.sh
+++
@@ -1,9 +0,0 @@
-curl --header "Content-Type: application/json" \
-  --request POST \
-  --data '{"title":"Cooler Titel", "category":"Sonstig","body":"leblos"}' \
-  http://localhost:3000/idea
-
-curl --header "Content-Type: application/json" \
-  --request POST \
-  --data '{"title":"Cooler Titel", "category":"","body":"leblos"}' \
-  http://localhost:3000/idea
--- ideaboard_app_backup/src/components/Footer.tsx
+++
@@ -1,9 +0,0 @@
-"use client"
-
-export const Footer = () => {
-    return (
-        <footer className="p-[8px] px-18 text-center border-t-2 border-solid  border-(--border)">
-            <h3>Impressum und so yay</h3>
-        </footer>
-    );
-};
